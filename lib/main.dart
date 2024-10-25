a1368b0d9ec440bb8f23b28f5263a4f750504a35
김동현 <71127040+DongHyeonKim-tech@users.noreply.github.com>
2024-10-25 11:33:06 +0900

    #6.1 AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,17 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-class Player {
-  // step 1
-  // String name = 'kim';
-
-  // step 2
-  // String name;
-  // Player(this.name);
-
-  // step 3
-  String? name;
-  Player({required this.name});
-}
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
-  // step 1
-  // var kim = Player();
-
-  // step 2
-  // var kim = Player('kim');
-
-  // step 3
-  var kim = Player(name: 'kim');
-
-  runApp(App());
+  runApp(const App());
 }
 
-// App은 material(google), cupertino(apple) 디자인 시스템 중 하나 return
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
-    // throw UnimplementedError();
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text('Hello flutter!')),
-          // centerTitle: false,
-          // 그림자
-          // elevation: 1,
-          // title: Text('Hello flutter'),
-          backgroundColor: Color.fromARGB(75, 4, 203, 173),
-        ),
-        // new를 쓰던 말던 상관없음
-        body: new Center(child: Text('Hello world!')),
-      ),
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
