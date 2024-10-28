d560712125f9b270609bc51a0f63cf6f198a9927
김동현 <71127040+DongHyeonKim-tech@users.noreply.github.com>
2024-10-28 14:42:07 +0900

    #6.5 waitForWebToons - HomeScreen Stateless - > Stateful 변경, 데이터 loading 개발

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon.dart';
+import 'package:toonflix/services/api_service.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<WebtoonModel> webtoons = [];
+  bool isLoading = true;
+
+  void waitForWebToons() async {
+    // 비동기 통신을 통해 받아온 데이터를 webtoons에 저장
+    webtoons = await ApiService.getTodaysToons();
+    // 데이터 loading false
+    isLoading = false;
+    // 화면 새로고침
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    waitForWebToons();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print(webtoons);
+    print(isLoading);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
