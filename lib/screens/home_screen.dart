f0b3122738e159fdb650f59462ded4b3f4fd6450
김동현 <71127040+DongHyeonKim-tech@users.noreply.github.com>
2024-10-28 16:01:41 +0900

    #6.8 Webtoon Card - ListView Method화(makeList), Webtoon Card style 추가

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -24,40 +24,84 @@ class HomeScreen extends StatelessWidget {
           future: webtoons,
           builder: (context, snapshot) {
             if (snapshot.hasData) {
-              // 1 - 가장 간단한 ListView
-              // return ListView(
-              //   children: [
-              //     for (var webtoon in snapshot.data!) Text(webtoon.title)
-              //   ],
-              // );
-              // 2 - index를 활용해 사용자가 현재 보고 있는 화면만 build, 나머지는 memory에서 delete
-              // return ListView.builder(
-              //   scrollDirection: Axis.vertical,
-              //   itemCount: snapshot.data!.length,
-              //   itemBuilder: (context, index) {
-              //     print(index);
-              //     var webtoon = snapshot.data![index];
-              //     return Text(webtoon.title);
-              //   },
-              // );
-              // 3 - List에 간격 설정
-              return ListView.separated(
-                scrollDirection: Axis.vertical,
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  print(index);
-                  var webtoon = snapshot.data![index];
-                  return Text(webtoon.title);
-                },
-                separatorBuilder: (context, index) => const SizedBox(
-                  height: 10,
-                ),
+              return Column(
+                children: [
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Expanded(child: makeList(snapshot))
+                ],
               );
             }
             return const Center(
               child: CircularProgressIndicator(),
             );
           }),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return
+        // 1 - 가장 간단한 ListView
+        // return ListView(
+        //   children: [
+        //     for (var webtoon in snapshot.data!) Text(webtoon.title)
+        //   ],
+        // );
+        // 2 - index를 활용해 사용자가 현재 보고 있는 화면만 build, 나머지는 memory에서 delete
+        // return ListView.builder(
+        //   scrollDirection: Axis.vertical,
+        //   itemCount: snapshot.data!.length,
+        //   itemBuilder: (context, index) {
+        //     print(index);
+        //     var webtoon = snapshot.data![index];
+        //     return Text(webtoon.title);
+        //   },
+        // );
+        // 3 - List에 간격 설정
+        ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: const EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 20,
+      ),
+      itemBuilder: (context, index) {
+        var webtoon = snapshot.data![index];
+        return Column(
+          children: [
+            Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: const [
+                  BoxShadow(
+                      blurRadius: 7,
+                      offset: Offset(10, 10),
+                      color: Colors.black54)
+                ],
+              ),
+              child: Image.network(
+                webtoon.thumb,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              webtoon.title,
+              style: const TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(
+        width: 20,
+      ),
     );
   }
 }
